55626464851eac6e4a794880b4aae818475bdb20
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-09 13:53:29 +0200

    great american wall

--- plansza/models.py
+++ plansza/models.py
@@ -1,11 +1,16 @@
+import requests
 from django.contrib.auth.models import User
 from django.db import models
 
+
+def get_random_photo():
+    return requests.head("https://source.unsplash.com/people/1500x550", allow_redirects=True).url
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
     facebook_id = models.BigIntegerField(null=True, blank=True, unique=True)
     description = models.TextField(null=True, blank=True)
+    image = models.URLField(default=get_random_photo)
 
 
 class EventHour(models.Model):
--- plansza/settings.py
+++ plansza/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = '_xx7ykcmm(rq@s+55vf2ma5rd6jhwt&menz5+^)b7kwbd0r3zq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -58,8 +57,8 @@ ROOT_URLCONF = 'plansza.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        "DEBUG": True,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
