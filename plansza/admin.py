e8a11456b7214eb8a18bc9f87fbab5e3a5f0394d
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-09 14:05:13 +0200

    now I could likely properly debug

---
+++ plansza/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Event, EventHour
+
+
+class EventHourInline(admin.StackedInline):
+    model = EventHour
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("pk", "facebook_id", "name", "description", "image")
+    inlines = (EventHourInline,)
+
+
+@admin.register(EventHour)
+class EventHourAdmin(admin.ModelAdmin):
+    list_display = ("event", "users")
