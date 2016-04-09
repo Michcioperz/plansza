010c22b76986055e6ede40eb9cb94dd479a2042e
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-09 15:27:18 +0200

    moved some stuff, also now adding person's all events to database now

--- plansza/utils.py
+++ plansza/utils.py
@@ -1,5 +1,19 @@
 import facebook
+import requests
+
+from plansza.models import Event
 
 
 def get_graph(user) -> facebook.GraphAPI:
     return facebook.GraphAPI(access_token=user.social_auth.get().access_token)
+
+
+def ensure_event_import(graph, ident: str):
+    try:
+        Event.objects.get(facebook_id=int(ident))
+    except Event.DoesNotExist:
+        event = graph.get_object(id=ident)
+        image = graph.get_connections(id=ident, connection_name="picture", type="large")
+        Event.objects.create(name=event["name"], description=event["description"], facebook_id=int(event["id"]), image=(
+            image["url"] if "url" in image else requests.head("https://source.unsplash.com/category/people/1500x550",
+                                                              allow_redirects=True).url))
--- plansza/views.py
+++ plansza/views.py
@@ -1,31 +1,21 @@
-import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
 from .models import Event
-from .utils import get_graph
-
-
-def ensure_event_import(request, ident: str):
-    try:
-        Event.objects.get(facebook_id=int(ident))
-    except Event.DoesNotExist:
-        event = get_graph(request.user).get_object(id=ident)
-        image = get_graph(request.user).get_connections(id=ident, connection_name="picture", type="large")
-        Event.objects.create(name=event["name"], description=event["description"], facebook_id=int(event["id"]), image=(
-            image["url"] if "url" in image else requests.head("https://source.unsplash.com/category/people/1500x550",
-                                                              allow_redirects=True).url))
+from .utils import get_graph, ensure_event_import
 
 
 @login_required
 def list_events(request):
     events = get_graph(request.user).get_connections(id="me", connection_name="events")["data"]
+    for event in events:
+        ensure_event_import(get_graph(request.user), event["id"])
     return render(request, "plansza/list_events.html", {"events": events})
 
 
 @login_required
 def event_details(request, ident: str):
-    ensure_event_import(request, ident)
+    ensure_event_import(get_graph(request.user), ident)
     event = get_object_or_404(Event, facebook_id=int(ident))
     return render(request, "plansza/event_details.html", {"event": event})
 
