f25742d0a1d56cad855685fd19d019bc9b8f07d6
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-10 04:52:32 +0200

    maybe something like this

--- plansza/models.py
+++ plansza/models.py
@@ -45,8 +45,10 @@ class Friend(models.Model):
     def update(self):
         with transaction.atomic():
             # TODO: this may not work for more than 15 or so friends
-            self.friends = [Friend.objects.get(user__social_auth__uid=x["id"]) for x in
-                            get_graph(self.user).get_connections("me", "friends")["data"]]
+            self.friends = Friend.objects.filter(user__social_auth__uid__in=[x["id"] for x in
+                                                                             get_graph(self.user).get_connections("me",
+                                                                                                                  "friends")[
+                                                                                 "data"]])
             self.image = get_graph(self.user).get_connections("me", "picture", type="large")["url"]
             self.save()
 
